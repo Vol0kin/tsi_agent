f219dc828e26a6960bd3f30fc571c2814a4a4494
Vladislav Nikolov <vladis890@gmail.com>
2019-02-19 14:39:13 +0100

    Added method to compute the shortest path between two Observations called shortestPathLength.

--- GVG-AI/src/practica_busqueda/TestAgent.java
+++ GVG-AI/src/practica_busqueda/TestAgent.java
@@ -412,7 +412,7 @@ public class TestAgent extends BaseAgent{
         // Guardar distancia recorrida  y acciones en la informacion del plan
         if (objetivoEncontrado) {
             System.out.println("Encontrado objetivo");
-            nuevoPlan.distancia = posInicial.getManhattanDistance(objetivo);
+            //nuevoPlan.distancia = posInicial.getManhattanDistance(objetivo);
         
             while (recorrido.padre != null) {
                 // Aniadir casillas recorridas
@@ -425,6 +425,7 @@ public class TestAgent extends BaseAgent{
             
             // Aniadir casilla inicial
             nuevoPlan.listaCasillas.add(0, recorrido.observacion);
+            nuevoPlan.distancia = nuevoPlan.listaCasillas.size();
         }  
         
         return nuevoPlan;
@@ -681,5 +682,134 @@ public class TestAgent extends BaseAgent{
             }
         
         return dist_matrix;
+    }
+    
+    private int shortestPathLength(Observation posInicial, Observation posFinal, StateObservation stateObs) {
+        int distancia = 0;
+        
+        // Cola de nodos abiertos
+        PriorityQueue<CasillaCamino> listaAbiertos = new PriorityQueue<>(
+                (CasillaCamino c1, CasillaCamino c2) -> c1.costeF - c2.costeF);
+        
+        // Lista de nodos explorados (cerrados)
+        ArrayList<CasillaCamino> listaExplorados = new ArrayList<>();
+        
+        // Observacion del mapa
+        ArrayList<Observation>[][] observacionNivel = this.getObservationGrid(stateObs);
+        
+        // Observacion actual a explorar
+        Observation observacionActual;
+        
+        // Representa el nodo que se explora a continuacion
+        CasillaCamino nodoActual;
+        
+        final ObservationType muro = ObservationType.WALL;
+        
+                
+        final int numFilas = observacionNivel.length,
+                  numColumnas = observacionNivel[0].length;
+        
+        // Mapa de casillas exploradas
+        boolean[][] mapaExplorado = new boolean[numFilas][numColumnas];
+        
+        for (int i = 0; i < numFilas; i++) {
+            for (int j = 0; j < numColumnas; j++) {
+                mapaExplorado[i][j] = false;
+            }
+        }
+        
+        boolean objetivoEncontrado = false;
+        
+        // Lista de acciones
+        ArrayList<Types.ACTIONS> listaAcciones = new ArrayList<>();
+                
+        listaAcciones.add(Types.ACTIONS.ACTION_UP);
+        listaAcciones.add(Types.ACTIONS.ACTION_DOWN);
+        listaAcciones.add(Types.ACTIONS.ACTION_LEFT);
+        listaAcciones.add(Types.ACTIONS.ACTION_RIGHT);
+        
+        // Lista de orientaciones
+        ArrayList<Orientation> listaOrientaciones = new ArrayList<>();
+        
+        listaOrientaciones.add(Orientation.N);
+        listaOrientaciones.add(Orientation.S);
+        listaOrientaciones.add(Orientation.W);
+        listaOrientaciones.add(Orientation.E);
+        
+        Orientation orientacionInicial;
+        
+        if (posFinal.getY() < posInicial.getY()) {
+            orientacionInicial = Orientation.N;
+        } else {
+            orientacionInicial = Orientation.S;
+        }
+        
+        listaAbiertos.add(new CasillaCamino(0, posFinal.getManhattanDistance(posInicial),
+                                            orientacionInicial, null, posInicial, null));
+        
+        mapaExplorado[posInicial.getX()][posInicial.getY()] = true;
+        
+        while (!listaAbiertos.isEmpty() && !objetivoEncontrado) {
+                       
+            nodoActual = listaAbiertos.poll();
+            
+            observacionActual = nodoActual.observacion;
+            
+            int xActual = observacionActual.getX(),
+                yActual = observacionActual.getY();
+            
+            if (posFinal.equals(observacionActual)) {
+                objetivoEncontrado = true;
+            } else {                
+                LinkedList<Types.ACTIONS> acciones;
+                
+                int yArriba = yActual - 1,
+                    yAbajo = yActual + 1,
+                    xIzquierda = xActual - 1,
+                    xDerecha = xActual + 1,
+                    costeHijo;  // Coste para ir al hijo (numero de acciones)
+                
+                ArrayList<Observation> casillasDescendientes = new ArrayList<>();
+                
+                casillasDescendientes.add(observacionNivel[xActual][yArriba].get(0));
+                casillasDescendientes.add(observacionNivel[xActual][yAbajo].get(0));
+                casillasDescendientes.add(observacionNivel[xIzquierda][yActual].get(0));
+                casillasDescendientes.add(observacionNivel[xDerecha][yActual].get(0));
+                
+                
+                for (int i = 0; i < casillasDescendientes.size(); i++) {
+                    Observation casillaHija = casillasDescendientes.get(i);
+                    
+                    int xCasilla = casillaHija.getX(),
+                        yCasilla = casillaHija.getY();
+                    
+                    if (!mapaExplorado[xCasilla][yCasilla] && !casillaHija.getType().equals(muro)) {
+                        costeHijo = nodoActual.costeG + 1;
+                    
+                        if (!nodoActual.orientacion.equals(listaOrientaciones.get(i))) {
+                            costeHijo++;
+                        }
+                    
+                        listaAbiertos.add(new CasillaCamino(costeHijo,
+                                                            posFinal.getManhattanDistance(observacionActual),
+                                                            listaOrientaciones.get(i), null, casillaHija, nodoActual));
+                    }
+                    
+                    mapaExplorado[xCasilla][yCasilla] = true;                    
+                }                
+            }                  
+            
+            listaExplorados.add(nodoActual);
+        }
+        
+        // Obtener la casilla del objetivo
+        CasillaCamino recorrido = listaExplorados.get(listaExplorados.size() - 1);
+        
+        while (recorrido != null) {
+            distancia++;
+            recorrido = recorrido.padre;
+        }
+        
+        return distancia;
     }
 }
